bed21f9ca31a65526f5dc4596737d3903a44dbd6
chiyenLee <johlee@g.hmc.edu>
2018-12-02 14:21:00 -0800

    add internal step counts, direction change (untested)

--- step_motor_drive.sv
+++ step_motor_drive.sv
@@ -1,7 +1,8 @@
 /* Stepper motor driver 
 johlee@g.hmc.edu Nov. 17, 2018 */
 
-module step_motor_drive(input logic clk, reset, dir, en,
+module step_motor_drive(input logic clk, reset, en, load,
+						input logic [3:0] digit,
 								//input logic [7:0] num_steps,
 								output logic A1, A2, B1, B2);
 	
@@ -10,17 +11,24 @@ module step_motor_drive(input logic clk, reset, dir, en,
 	// if en = 1, move motor. otherwise hold
 	
 	logic increment, stop; // flag to tell to count the step or not
+	logic [3:0] current_digit;
 	logic [18:0] q;
-	logic [13:0] steps; 
+	logic signed [13:0] steps; 
+	logic signed [13:0] delta_steps;
 	
 	// temporary
-	logic [13:0] num_steps;
+	logic signed [13:0] num_steps;
 	logic slow_clk;
 	
 	assign slow_clk = q[18];
-	
-	assign num_steps = 14'd220;
-	
+	assign delta_steps = steps - num_steps; // steps to increment
+	assign dir = delta_steps[13];
+
+	// store digit 
+	always_ff @(posedge clk)
+		if (load) current_digit <= digit;
+		else current_digit <= current_digit; 
+
 	// modulate a slower clock
 	always_ff @(posedge clk, posedge reset)
 		if (reset) q <= 0;
@@ -35,7 +43,7 @@ module step_motor_drive(input logic clk, reset, dir, en,
 		if (reset) begin state <= S0; steps <= 0; end
 		else begin
 			state <= nextstate;
-			if (increment) steps <= steps + 14'd1;
+			if (increment) steps <= steps + {dir, 13'sd1};
 		end
 	
 	assign stop = (steps >= num_steps) ? 1'b1 : 1'b0;
@@ -75,6 +83,22 @@ module step_motor_drive(input logic clk, reset, dir, en,
 			S3: begin {A1, B1, A2, B2} = 4'b0011; increment = 0; end
 			S4: begin {A1, B1, A2, B2} = 4'b1001; if (dir) increment = 1; else increment = 0; end
 		endcase
+
+	// run different number of steps according to the digit
+	always_comb 
+		case(current_digit)
+			4'd0: num_steps = 14'sd0;
+			4'd1: num_steps = 14'sd0;
+			4'd2: num_steps = 14'sd50;
+			4'd3: num_steps = 14'sd50;
+			4'd4: num_steps = 14'sd100;
+			4'd5: num_steps = 14'sd100;
+			4'd6: num_steps = 14'sd150;
+			4'd7: num_steps = 14'sd150;
+			4'd8: num_steps = 14'sd200;
+			4'd9: num_steps = 14'sd200;
+		endcase
+
 endmodule 
 
 module testbench();
