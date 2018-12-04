c785cfa518e83f3759a677d11504e9739c6330cf
John Lee <johlee@g.hmc.edu>
2018-12-03 21:34:05 -0800

    add switch flipping and delay module.

--- step_motor_drive.sv
+++ step_motor_drive.sv
@@ -3,18 +3,21 @@ johlee@g.hmc.edu Nov. 17, 2018 */
 
 module step_motor_drive(input logic clk, reset, en, load,
 								input logic [3:0] digit,
-								output logic A1, A2, B1, B2);
+								output logic A1, A2, B1, B2,
+								output logic PWM1, PWM2);
 	
 	// if dir = 1, move forward 
 	// if dir = 0, move backward
 	// if en = 1, move motor. otherwise hold
-	
-	logic increment, stop, dir, stop_flag; // flag to tell to count the step or not
+		
+	logic increment, stop, dir, stop_flag, position_reached; // flag to tell to count the step or not
 	logic [3:0] current_digit;
 	logic [18:0] q;
 	logic signed [13:0] steps; 
 	logic signed [13:0] delta_steps;
 	logic signed [13:0] step_increment;
+	
+	servo_control s1(clk, reset, position_reached, digit, PWM1, PWM2);
 	
 	// temporary
 	logic signed [13:0] num_steps;
@@ -36,7 +39,7 @@ module step_motor_drive(input logic clk, reset, en, load,
 		else q <= q + 19'd1; 
 	
 	// define state that controls the steps 
-	typedef enum logic [4:0] {S0, S1, S2, S3, S4} statetype;
+	typedef enum logic [4:0] {S0, S1, S2, S3, S4, hit} statetype;
 	statetype state, nextstate; 
 	
 	// state transition
@@ -63,7 +66,7 @@ module step_motor_drive(input logic clk, reset, en, load,
 				 else nextstate = S0;
 			S1: if (en & dir & ~stop) nextstate = S2;
 				 else if (en & ~dir & ~stop) nextstate = S4; 
-				 else if (stop) nextstate = S0;
+				 else if (stop) nextstate = hit;
 				 else nextstate = S0;
 			S2: if (en & dir & ~stop) nextstate = S3;
 				 else if (en & ~dir & ~stop) nextstate = S1; 
@@ -75,19 +78,22 @@ module step_motor_drive(input logic clk, reset, en, load,
 				 else nextstate = S0;
 			S4: if (en & dir & ~stop) nextstate = S1;
 				 else if (en & ~dir & ~stop) nextstate = S3; 
-				 else if (stop) nextstate = S0;
+				 else if (stop) nextstate = hit;
 				 else nextstate = S0;
+			hit: nextstate = S0;
 			default: nextstate = S0; // always go back to S0 if enable is off
 		endcase
 	
 	// output logic 
 	always_comb
 		case(state)
-			S0: begin {A1, B1, A2, B2} = 4'b0000; increment = 0; end
-			S1: begin {A1, B1, A2, B2} = 4'b1100; if (~dir) increment = 1; else increment = 0; end
-			S2: begin {A1, B1, A2, B2} = 4'b0110; increment = 0; end
-			S3: begin {A1, B1, A2, B2} = 4'b0011; increment = 0; end
-			S4: begin {A1, B1, A2, B2} = 4'b1001; if (dir) increment = 1; else increment = 0; end
+			S0: begin {A1, B1, A2, B2} = 4'b0000; increment = 0; position_reached = 0; end
+			S1: begin {A1, B1, A2, B2} = 4'b1100; if (~dir) increment = 1; else increment = 0; position_reached = 0; end
+			S2: begin {A1, B1, A2, B2} = 4'b0110; increment = 0; position_reached = 0;end
+			S3: begin {A1, B1, A2, B2} = 4'b0011; increment = 0; position_reached = 0; end
+			S4: begin {A1, B1, A2, B2} = 4'b1001; if (dir) increment = 1; else increment = 0; position_reached = 0; end
+			hit: begin {A1, B1, A2, B2} = 4'b1001; increment = 0; position_reached = 1; end
+			default: position_reached = 0;
 		endcase
 
 	// run different number of steps according to the digit
@@ -105,7 +111,61 @@ module step_motor_drive(input logic clk, reset, en, load,
 			4'd9: num_steps = 14'sd200;
 			default: num_steps = 14'sd0;
 		endcase
+		
+endmodule 
 
+module servo_control(input logic clk,
+							input logic reset,
+							input logic position_reached,
+							input logic [3:0] digit,
+							output logic servo_pwm1,
+							output logic servo_pwm2);
+		
+		logic [19:0] duty_cycle1, duty_cycle2, count, rest_dutycycle, engaged_dutycycle1, engaged_dutycycle2;
+		logic [31:0] delay_count; 
+		logic is_left_servo;
+		logic engage, start_engaging; // delay it long enough for the servo to act
+		
+		// wait for a bit before hitting
+		// TODO: fix this delay. it's a bit unelegant.
+		always_ff @(posedge clk)
+			if (reset) delay_count = 0;
+			else if (position_reached) delay_count <= 0;
+			else if ( ~(delay_count == 31'd40000001)) delay_count <= delay_count + 1;
+			else delay_count <= delay_count;
+		
+		// engage 
+		assign engage = (delay_count >= 31'd20000000 & delay_count <= 31'd40000000);			
+		
+		assign rest_dutycycle = 20'd64000;
+		assign engaged_dutycycle1 = 20'd48000;
+		assign engaged_dutycycle2 = 20'd80000;
+
+		assign is_left_servo = (digit % 2) ? 1 : 0;
+		
+		// establishing a 20ms update rate
+		always_ff @(posedge clk)
+			if (reset || count == 20'd800000) count = 20'b0;
+			else count <= count + 1;
+		
+		// creating pwm 
+		always_ff @(posedge clk)
+			if (count < duty_cycle1) servo_pwm1 = 1'b1; 
+			else servo_pwm1 = 1'b0;
+			
+		always_ff @(posedge clk)
+			if (count < duty_cycle2) servo_pwm2 = 1'b1;
+			else servo_pwm2 = 1'b0;
+			
+		// select the duty cycle 
+		always_comb 
+			case(engage)
+				1'b1: if (is_left_servo) begin duty_cycle1 = engaged_dutycycle1; duty_cycle2 = rest_dutycycle; end
+						else begin duty_cycle2 = engaged_dutycycle2; duty_cycle1 = rest_dutycycle; end
+				default: begin duty_cycle1 = rest_dutycycle; duty_cycle2 = rest_dutycycle; end
+			endcase
+			
+		
 endmodule 
 
 module testbench();
@@ -114,8 +174,9 @@ module testbench();
 	logic [3:0] stepper_out;
 	logic [8:0] count;
 	logic [7:0] num_steps;
+	logic PWM1, PWM2;
 	
-	step_motor_drive dut(clk, reset, en, load, digit, stepper_out[3], stepper_out[2], stepper_out[1], stepper_out[0]);
+	step_motor_drive dut(clk, reset, en, load, digit, stepper_out[3], stepper_out[2], stepper_out[1], stepper_out[0], PWM1, PWM2);
 	
 	initial 
 		forever begin
